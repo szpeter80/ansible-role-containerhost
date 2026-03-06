fcc7d9298a7f5f4ce1b990ad777438f6ef3333c2
szpeter80 <szpeter80@users.noreply.github.com>
2026-03-06 23:12:22 +0100

    + add new projects gitlab + redmine

---
+++ files/container-projects/nw_haproxy/TODO.md
@@ -0,0 +1,6 @@
+TODO after Ansible run
+----------------------
+
+- update gen-snakeoil.sh with your domain, generate certs
+- update url paths in haproxy.cfg
+- execute svc.sh start
--- files/container-projects/nw_haproxy/gen-snakeoil-cert.sh
+++ files/container-projects/nw_haproxy/gen-snakeoil-cert.sh
@@ -6,7 +6,7 @@ openssl req -new \
     -subj "/C=US/ST=Krakosia/L=City/O=ACME corp/CN=*.snakeoil.example.com" \
     -addext "subjectAltName=DNS:snakeoil.example.com,DNS:*.snakeoil.example.com,IP:192.168.1.1"
 
-# Blindly copying extensions from the CRT is a bad practice for a real CA, but will do for a dummy one
+# Blindly copying extensions from the CSR is a bad practice for a real CA, but will do for a dummy one
 openssl x509 -req -days 3650 -in ./certs/snakeoil.csr -signkey ./certs/snakeoil.key -out ./certs/snakeoil.crt -copy_extensions copyall
 
 cat ./certs/snakeoil.crt >> ./certs/snakeoil.pem
