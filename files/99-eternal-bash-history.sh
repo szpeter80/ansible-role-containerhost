c4ea5d0383d859ec787c0fdf2d6411e55a6a30ba
szpeter80 <szpeter80@users.noreply.github.com>
2026-04-05 14:14:17 +0200

    + add eternal bash history

---
+++ files/99-eternal-bash-history.sh
@@ -0,0 +1,19 @@
+# shellcheck disable=SC2148
+#
+# Eternal bash history
+# ---------------------
+#
+# The timestamps are stored as epoch times and will be formatted only
+# when history is checked with the "history" command
+#
+# Undocumented feature which sets the size to "unlimited".
+# http://stackoverflow.com/questions/9457233/unlimited-bash-history
+export HISTFILESIZE=
+export HISTSIZE=
+export HISTTIMEFORMAT="[%F %T] "
+# Change the file location because certain bash sessions truncate .bash_history file upon close.
+# http://superuser.com/questions/575479/bash-history-truncated-to-500-lines-on-each-login
+export HISTFILE=~/.bash_eternal_history
+# Force prompt to write history after every command.
+# http://superuser.com/questions/20900/bash-history-loss
+PROMPT_COMMAND="history -a; $PROMPT_COMMAND"
